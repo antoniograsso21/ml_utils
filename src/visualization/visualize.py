fad0a199cc43b617716a57edcd9851915b8e78ea
antoniograsso21 <antoniograsso21@gmail.com>
2022-11-13 19:42:24 +0100

    added visualization functions

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,10 +1,40 @@
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 class Visualization:
 
+    @staticmethod
+    def set_figure_size(width, height):
+        plt.figure(figsize=(width, height))
+
+    @staticmethod
+    def set_title(title: str):
+        plt.title(title)
+
+    @staticmethod
+    def set_xlabel(label: str):
+        plt.xlabel(label)
+
+    @staticmethod
+    def set_ylabel(label: str):
+        plt.ylabel(label)
+
+    @staticmethod
+    def plot_line_chart(df: pd.DataFrame):
+        sns.lineplot(data=df)
+
+    @staticmethod
+    def plot_bar_chart(xcolumn: pd.Series, ycolumn: pd.Series):
+        sns.barplot(x=xcolumn, y=ycolumn)
+
+    @staticmethod
+    def plot_heatmap(df: pd.DataFrame, show_cell_values: bool = True):
+        sns.heatmap(data=df, annot=show_cell_values)
+
+    @staticmethod
     def plot_mi_scores(scores):
         scores = scores.sort_values(ascending=True)
         width = np.arange(len(scores))
@@ -13,6 +43,7 @@ class Visualization:
         plt.yticks(width, ticks)
         plt.title("Mutual Information Scores")
 
+    @staticmethod
     def plot_variance(pca, width=8, dpi=100):
         # Create figure
         fig, axs = plt.subplots(1, 2)
@@ -31,9 +62,10 @@ class Visualization:
             xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
         )
         # Set up figure
-        fig.set(figwidth=8, dpi=100)
+        fig.set(figwidth=width, dpi=dpi)
         return axs
 
+    @staticmethod
     def corrplot(df, method="pearson", annot=True, **kwargs):
         sns.clustermap(
             df.corr(method),
